d2f567069ed8d18f0ab84e446d0529a7cd8a8446
Ayh2k <ayhan.ahsan@gmail.com>
2024-10-24 00:44:44 -0400

    - updates

---
+++ JavaFX-CRUD-application/src/application/CustomerController.java
@@ -0,0 +1,20 @@
+package application;
+
+import javafx.scene.layout.Region;
+import javafx.util.Builder;
+
+public class CustomerController {
+	
+	private Builder<Region> viewBuilder;
+	private CustomerInteractor interactor;
+	
+	public CustomerController() {
+		CustomerModel model = new CustomerModel();
+		viewBuilder = new CustomerViewBuilder(model);
+		interactor = new CustomerInteractor(model);
+	}
+	
+	public Region getView() {
+		return viewBuilder.build();
+	}
+}
---
+++ JavaFX-CRUD-application/src/application/CustomerInteractor.java
@@ -0,0 +1,10 @@
+package application;
+
+public class CustomerInteractor {
+	
+	private CustomerModel model;
+	
+	public CustomerInteractor(CustomerModel model) {
+		this.model = model;
+	}
+}
